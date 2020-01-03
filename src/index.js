bef3c6a9caf4b16cbdbb51773445bdef68e29098
alex0735070005 <alex0735070005@gmail.com>
2020-01-03 14:47:45 +0200

    start-project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import 'core-js';
+import prefetchWrapper from './prefetch';
+
+/**
+ * Creates a list of decorators
+ *
+ * @return {Object} - connect wrappers
+ */
+export default prefetchWrapper;
+
+export serverPreload from './prefetch/serverPrefetch';
